0b6896f8c6751c3c508a3ac1282fd973cf581bdc
thephilipquan <thephilipquan@gmail.com>
2023-11-03 12:12:45 -0700

    ignore sh scripts

--- assignment-02/get.sh
+++
@@ -1,3 +0,0 @@
-#/usr/bin/bash
-
-curl http://ec2-52-27-209-162.us-west-2.compute.amazonaws.com:8080/server_war/albums?albumId=$1
--- assignment-02/post.sh
+++
@@ -1,7 +0,0 @@
-#/usr/bin/bash
-
-curl http://localhost:8080/api/albums \
-	-X POST \
-	-H "Content-Type: multipart/form-data" \
-	-F image=@bollocks.png \
-	-F profile='{"artist": "Sex Pistols", "title": "Never Mind the Bollocks", "year": 1983}'
