a4db48db1b5911abb90bd417bda1bc902fac4cee
thephilipquan <thephilipquan@gmail.com>
2023-11-03 13:01:00 -0700

    refactor: statistics and statics to main

--- assignment-02/client/src/main/java/org/philipquan/ClientApp.java
+++ assignment-02/client/src/main/java/org/philipquan/ClientApp.java
@@ -1,14 +1,11 @@
 package org.philipquan;
 
 import java.io.IOException;
-import java.util.Comparator;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -24,22 +21,12 @@ import org.apache.http.util.EntityUtils;
 
 public class ClientApp {
 
-    private static final String FORM_IMAGE_KEY = "image";
-    private static final String FORM_ALBUM_INFO_KEY = "profile";
-    private static final Pattern ALBUMID_PATTERN = Pattern.compile("albumId.*?(\\d+)");
-    private static final String ENDPOINT = "/albums";
-    private static final Integer INITIAL_THREAD_COUNT = 10;
-    private static final Integer INITIAL_REQUEST_COUNT = 100;
-    private static final Integer GROUP_REQUEST_COUNT = 1000;
-    private static final Integer REQUEST_RETRY_COUNT = 5;
-
     private final Integer groupThreadCount;
     private final Integer groupCount;
     private final Integer delayInSeconds;
     private final String hostUrl;
     private final String image;
     private final List<RequestStatistic> methodStatistics;
-    private final CloseableHttpClient client;
 
     public ClientApp(Integer groupThreadCount, Integer groupCount, Integer delayInSeconds, String hostUrl, String image, List<RequestStatistic> requestStatistics) {
         this.groupThreadCount = groupThreadCount;
@@ -48,9 +35,6 @@ public class ClientApp {
         this.hostUrl = hostUrl;
         this.image = image;
         this.methodStatistics = requestStatistics;
-        this.client = HttpClientBuilder.create()
-          .setRetryHandler(new StandardHttpRequestRetryHandler(REQUEST_RETRY_COUNT, true))
-          .build();
     }
 
     /**
@@ -58,9 +42,11 @@ public class ClientApp {
      * {@link HttpStatus#SC_OK} and {@link HttpStatus#SC_BAD_REQUEST}.
      */
     public Boolean hostUrlExists() {
-        HttpGet request = new HttpGet(this.hostUrl + ENDPOINT);
-        try {
-            int statusCode = this.client.execute(request).getStatusLine().getStatusCode();
+        HttpGet request = new HttpGet(this.hostUrl + Main.ENDPOINT);
+        try (
+          CloseableHttpClient client = createClient();
+          ) {
+            int statusCode = client.execute(request).getStatusLine().getStatusCode();
             return statusCode >= HttpStatus.SC_OK && statusCode <= HttpStatus.SC_BAD_REQUEST;
         } catch (IOException e) {
             return false;
@@ -68,8 +54,8 @@ public class ClientApp {
     }
 
     public void initialRun() {
-        CountDownLatch latch = new CountDownLatch(INITIAL_THREAD_COUNT);
-        processThreads(INITIAL_THREAD_COUNT, INITIAL_REQUEST_COUNT, latch);
+        CountDownLatch latch = new CountDownLatch(Main.INITIAL_THREAD_COUNT);
+        processThreads(Main.INITIAL_THREAD_COUNT, Main.INITIAL_REQUEST_COUNT, latch);
         try {
             latch.await();
             System.out.println("Initial run completed.");
@@ -83,7 +69,7 @@ public class ClientApp {
         CountDownLatch latch = new CountDownLatch(this.groupCount * this.groupThreadCount);
         IntStream.range(0, this.groupCount).forEach(i -> {
             System.out.println("Processing group: " + i + "...");
-            processThreads(this.groupThreadCount, GROUP_REQUEST_COUNT, latch);
+            processThreads(this.groupThreadCount, Main.GROUP_REQUEST_COUNT, latch);
             try {
                 Thread.sleep(this.delayInSeconds * 1000);
             } catch (InterruptedException e) {
@@ -107,32 +93,38 @@ public class ClientApp {
      *                    {@code requestCount} amount of times
      */
     public void processThreads(int threadCount, int requestCount, CountDownLatch latch) {
-        IntStream.range(0, threadCount).forEach(i -> {
-            Runnable instruction = () -> {
-                callServer(requestCount);
-                latch.countDown();
-            };
-            new Thread(instruction).start();
-        });
+        try (
+          CloseableHttpClient client = createClient();
+        ) {
+            IntStream.range(0, threadCount).forEach(i -> {
+                Runnable instruction = () -> {
+                    callServer(client, requestCount);
+                    latch.countDown();
+                };
+                new Thread(instruction).start();
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-    private void callServer(int requestCount) {
+    private void callServer(CloseableHttpClient client, int requestCount) {
         IntStream.range(0, requestCount).forEach(j -> {
-//            Integer albumId = postAlbum();
+//            Integer albumId = postAlbum(client);
             Integer albumId = 1;
-            getAlbum(albumId);
+            getAlbum(client, albumId);
         });
     }
 
     /**
      * @return The id of the newly posted album.
      */
-    private Integer postAlbum() {
-        HttpPost request = new HttpPost(this.hostUrl + ENDPOINT);
+    private Integer postAlbum(CloseableHttpClient client) {
+        HttpPost request = new HttpPost(this.hostUrl + Main.ENDPOINT);
 
         HttpEntity entity = MultipartEntityBuilder.create()
-          .addPart(FORM_IMAGE_KEY, new StringBody(image, ContentType.TEXT_PLAIN))
-          .addPart(FORM_ALBUM_INFO_KEY, new StringBody("{\"artist\": \"joe\", \"title\": \"joe's story\", \"year\": 2023}", ContentType.TEXT_PLAIN))
+          .addPart(Main.FORM_IMAGE_KEY, new StringBody(image, ContentType.TEXT_PLAIN))
+          .addPart(Main.FORM_ALBUM_INFO_KEY, new StringBody("{\"artist\": \"joe\", \"title\": \"joe's story\", \"year\": 2023}", ContentType.TEXT_PLAIN))
           .build();
         request.setEntity(entity);
 
@@ -141,7 +133,7 @@ public class ClientApp {
         CloseableHttpResponse response = null;
         try {
             timer.start();
-            response = this.client.execute(request);
+            response = client.execute(request);
             timer.stop();
             responseBody = EntityUtils.toString(response.getEntity());
             this.methodStatistics.add(RequestStatistic.createPost(response.getStatusLine().getStatusCode(), timer));
@@ -152,67 +144,32 @@ public class ClientApp {
             safeCloseResponse(response);
         }
 
-        Matcher match = ALBUMID_PATTERN.matcher(responseBody);
+        Matcher match = Main.ALBUMID_PATTERN.matcher(responseBody);
         if (!match.find()) {
             throw new RuntimeException(responseBody);
         }
         return Integer.parseInt(match.group(1));
     }
 
-    private void getAlbum(Integer albumId) {
-        HttpGet request = new HttpGet(this.hostUrl + ENDPOINT + "?albumId=" + albumId);
+    private void getAlbum(CloseableHttpClient client, Integer albumId) {
+        HttpGet request = new HttpGet(this.hostUrl + Main.ENDPOINT + "?albumId=" + albumId);
         Timer timer = new Timer();
         CloseableHttpResponse response = null;
         try {
             timer.start();
-            response = this.client.execute(request);
+            response = client.execute(request);
             timer.stop();
             this.methodStatistics.add(RequestStatistic.createGet(response.getStatusLine().getStatusCode(), timer));
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
             request.releaseConnection();
-            safeCloseResponse(response);
         }
     }
 
-    private void safeCloseResponse(CloseableHttpResponse response) {
-        try {
-            response.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void reportStatistics(Timer timer) {
-        final double runtimeInSeconds = (double) timer.getElapsedTime() / 1000;
-        final long throughput = Math.round((this.groupCount * this.groupThreadCount * GROUP_REQUEST_COUNT
-          * 2) / runtimeInSeconds);
-        System.out.println("Wall Time: " + runtimeInSeconds + " second(s)");
-        System.out.println("Throughput: " + throughput + " requests per second");
-
-        this.methodStatistics.sort(Comparator.comparingLong(RequestStatistic::getLatency));
-        final long minLatency = this.methodStatistics.get(0).getLatency();
-        System.out.println("Min latency: " + minLatency);
-
-        final long maxLatency = this.methodStatistics.get(this.methodStatistics.size() - 1).getLatency();
-        System.out.println("Max latency: " + maxLatency);
-
-        final long meanLatency = this.methodStatistics.stream()
-          .map(RequestStatistic::getLatency)
-          .reduce(0L, Long::sum) / methodStatistics.size();
-        System.out.println("Mean latency: " + meanLatency);
-
-        final int middleIndex = this.methodStatistics.size() / 2;
-        final long medianLatency = this.methodStatistics.get(middleIndex).getLatency();
-        System.out.println("Median latency: " + medianLatency);
-
-        final int percentileIndex = (int) Math.ceil(0.99 * this.methodStatistics.size());
-        if (percentileIndex >= this.methodStatistics.size()) {
-            System.out.println("percentile index is too large");
-        } else {
-            final long percentile99Latency = this.methodStatistics.get(percentileIndex - 1).getLatency();
-            System.out.println("99th Percentile latency: " + percentile99Latency);
-        }
+    private CloseableHttpClient createClient() {
+        return HttpClientBuilder.create()
+          .setRetryHandler(new StandardHttpRequestRetryHandler(Main.REQUEST_RETRY_COUNT, true))
+          .build();
     }
 }
--- assignment-02/client/src/main/java/org/philipquan/Main.java
+++ assignment-02/client/src/main/java/org/philipquan/Main.java
@@ -5,22 +5,35 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class Main {
 
+    public static final String FORM_IMAGE_KEY = "image";
+    public static final String FORM_ALBUM_INFO_KEY = "profile";
+    public static final Pattern ALBUMID_PATTERN = Pattern.compile("albumId.*?(\\d+)");
+    public static final String ENDPOINT = "/albums";
+    public static final Integer INITIAL_THREAD_COUNT = 10;
+    public static final Integer INITIAL_REQUEST_COUNT = 100;
+    public static final Integer GROUP_REQUEST_COUNT = 1000;
+    public static final Integer REQUEST_RETRY_COUNT = 5;
+
     public static void main(final String[] args) {
         if (args.length != 5) {
-            throw new RuntimeException("Please provide 5 arguments to run the program. (threadCount, groupCount, delayInSeconds, hostUrl, imagePath)");
+            throw new RuntimeException("Please provide 5 arguments to run the program. (groupThreadCount, groupCount, delayInSeconds, hostUrl, imagePath)");
         }
 
-        final int threadCount = Integer.parseInt(args[0]);
+        final int groupThreadCount = Integer.parseInt(args[0]);
         final int groupCount = Integer.parseInt(args[1]);
         final int delayInSeconds = Integer.parseInt(args[2]);
         final String hostUrl = args[3];
         final String imagePath = args[4];
 
         String image = readFileAsString(imagePath);
-        ClientApp client = new ClientApp(threadCount, groupCount, delayInSeconds, hostUrl, image, Collections.synchronizedList(new ArrayList<RequestStatistic>()));
+        List<RequestStatistic> requestStatistics = Collections.synchronizedList(new ArrayList<>());
+        ClientApp client = new ClientApp(groupThreadCount, groupCount, delayInSeconds, hostUrl, image, requestStatistics);
         if (!client.hostUrlExists()) {
             throw new RuntimeException("Initial connection to host url: " + hostUrl + " failed.");
         }
@@ -31,7 +44,7 @@ public class Main {
         timer.start();
         client.groupRun();
         timer.stop();
-        client.reportStatistics(timer);
+        reportStatistics(requestStatistics, groupCount, groupThreadCount, timer);
     }
 
     private static String readFileAsString(String path) {
@@ -39,6 +52,38 @@ public class Main {
             return new String(Files.readAllBytes(Paths.get(path)));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void reportStatistics(List<RequestStatistic> requestStatistics, int groupCount, int groupThreadCount, Timer timer) {
+        final double runtimeInSeconds = (double) timer.getElapsedTime() / 1000;
+        final long throughput = Math.round((groupCount * groupThreadCount * GROUP_REQUEST_COUNT
+          * 2) / runtimeInSeconds);
+        System.out.println("Wall Time: " + runtimeInSeconds + " second(s)");
+        System.out.println("Throughput: " + throughput + " requests per second");
+
+        requestStatistics.sort(Comparator.comparingLong(RequestStatistic::getLatency));
+        final long minLatency = requestStatistics.get(0).getLatency();
+        System.out.println("Min latency: " + minLatency);
+
+        final long maxLatency = requestStatistics.get(requestStatistics.size() - 1).getLatency();
+        System.out.println("Max latency: " + maxLatency);
+
+        final long meanLatency = requestStatistics.stream()
+          .map(RequestStatistic::getLatency)
+          .reduce(0L, Long::sum) / requestStatistics.size();
+        System.out.println("Mean latency: " + meanLatency);
+
+        final int middleIndex = requestStatistics.size() / 2;
+        final long medianLatency = requestStatistics.get(middleIndex).getLatency();
+        System.out.println("Median latency: " + medianLatency);
+
+        final int percentileIndex = (int) Math.ceil(0.99 * requestStatistics.size());
+        if (percentileIndex >= requestStatistics.size()) {
+            System.out.println("percentile index is too large");
+        } else {
+            final long percentile99Latency = requestStatistics.get(percentileIndex - 1).getLatency();
+            System.out.println("99th Percentile latency: " + percentile99Latency);
         }
     }
 }
