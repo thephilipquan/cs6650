85e3c3e163b485bb0950705de9c60da5cfe98ae4
thephilipquan <thephilipquan@gmail.com>
2023-11-10 17:31:24 -0800

    revert schema to postgresql

--- assignment-02/schema.sql
+++ assignment-02/schema.sql
@@ -1,26 +1,12 @@
-CREATE DATABASE IF NOT EXISTS AlbumApp;
-USE AlbumApp;
+CREATE SCHEMA IF NOT EXISTS album_app;
+SET search_path TO album_app;
 
-DROP TABLE IF EXISTS Albums;
+DROP TABLE IF EXISTS albums;
 
-CREATE TABLE Albums(
-	id INT AUTO_INCREMENT,
-	artist VARCHAR(255) NOT NULL,
-	title VARCHAR(255) NOT NULL,
-	year INT NOT NULL,
-	image BLOB NOT NULL,
-	CONSTRAINT pk_Albums_id PRIMARY KEY (id)
+CREATE TABLE albums(
+	id serial PRIMARY KEY,
+	artist varchar(255) NOT NULL,
+	title varchar(255) NOT NULL,
+	year integer NOT NULL,
+	image bytea NOT NULL
 );
-
-# CREATE SCHEMA IF NOT EXISTS album_app;
-# SET search_path TO album_app;
-
-# DROP TABLE IF EXISTS albums;
-
-# CREATE TABLE albums(
-# 	id serial PRIMARY KEY,
-# 	artist varchar(255) NOT NULL,
-# 	title varchar(255) NOT NULL,
-# 	year integer NOT NULL,
-# 	image bytea NOT NULL
-# );
