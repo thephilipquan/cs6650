e014ca07a97ab6bf4edeeab1cb419f6ddbbcfe8a
thephilipquan <thephilipquan@gmail.com>
2023-11-29 10:54:08 -0800

    switch dependency from commons.dbcp to tomcat.jdbc

--- assignment-02/server/src/main/java/org/philipquan/dal/ConnectionManager.java
+++ assignment-02/server/src/main/java/org/philipquan/dal/ConnectionManager.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Properties;
-import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.apache.tomcat.jdbc.pool.PoolProperties;
 
 public class ConnectionManager {
 
-    private final BasicDataSource dataSource;
+    private final DataSource dataSource;
 
     public ConnectionManager() {
         Properties properties = new Properties();
@@ -18,14 +19,30 @@ public class ConnectionManager {
             throw new RuntimeException("Error initializing ConnectionManager...", e);
         }
 
-        this.dataSource = new BasicDataSource();
-        this.dataSource.setDriverClassName(properties.getProperty("driver"));
-        this.dataSource.setUrl(properties.getProperty("url"));
-        this.dataSource.setUsername(properties.getProperty("user"));
-        this.dataSource.setPassword(properties.getProperty("password"));
-        this.dataSource.setMinIdle(2); // MAYBE delete.
-        this.dataSource.setMaxIdle(20); // MAYBE delete.
-        this.dataSource.setMaxActive(40);
+        PoolProperties p = new PoolProperties();
+        p.setUrl(properties.getProperty("url"));
+        p.setDriverClassName(properties.getProperty("driver"));
+        p.setUsername(properties.getProperty("user"));
+        p.setPassword(properties.getProperty("password"));
+        p.setJmxEnabled(true);
+        p.setTestWhileIdle(false);
+        p.setTestOnBorrow(true);
+        p.setValidationQuery("SELECT 1");
+        p.setTestOnReturn(false);
+        p.setValidationInterval(30000);
+        p.setTimeBetweenEvictionRunsMillis(30000);
+        p.setMaxActive(30);
+        p.setInitialSize(10);
+        p.setMaxWait(10000);
+        p.setRemoveAbandonedTimeout(60);
+        p.setMinEvictableIdleTimeMillis(30000);
+        p.setMinIdle(10);
+        p.setMaxIdle(10);
+        p.setLogAbandoned(true);
+        p.setRemoveAbandoned(true);
+
+        this.dataSource = new DataSource();
+        this.dataSource.setPoolProperties(p);
     }
 
     public Connection getConnection() throws SQLException {
